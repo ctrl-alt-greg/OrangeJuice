53d29f67535de897f214164257d84f3ab7e99965
ctrl-greg <65520831+ctrl-greg@users.noreply.github.com>
2020-08-09 17:00:28 -0400

    ORange.cs - added data annotations

--- OrangeJuice/Models/Orange.cs
+++ OrangeJuice/Models/Orange.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace OrangeJuice.Models
 {
@@ -14,10 +15,16 @@ namespace OrangeJuice.Models
 		
 		public string Farm { get; set; }
 		
+		// Specifies Display Name
+		[Display(Name = "Harvest Date")]
 		// Specifies DateTime to just include date
 		[DataType(DataType.Date)]
 		public DateTime HarvestDate { get; set; }
 
+		// specifies wieght as decimal(precision, scale)
+		// precision is the max number of digits needed to express a single weight value
+		// scale is the max number of decimals needed
+		[Column(TypeName = "decimal(18, 2)")]
 		public decimal Weight { get; set; }
 	}
 }
