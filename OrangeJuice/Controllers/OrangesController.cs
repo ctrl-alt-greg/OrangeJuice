2124e11503c94242586f255b1a7f118caa8e8e60
ctrl-greg <65520831+ctrl-greg@users.noreply.github.com>
2020-08-09 15:03:11 -0400

    added gitignore

--- OrangeJuice/Controllers/OrangesController.cs
+++ OrangeJuice/Controllers/OrangesController.cs
@@ -10,144 +10,145 @@ using OrangeJuice.Models;
 
 namespace OrangeJuice.Controllers
 {
-    public class OrangesController : Controller
-    {
-        private readonly OrangeJuiceContext _context;
+	public class OrangesController : Controller
+	{
+		private readonly OrangeJuiceContext _context;
 
-        public OrangesController(OrangeJuiceContext context)
-        {
-            _context = context;
-        }
+		public OrangesController(OrangeJuiceContext context)
+		{
+			_context = context;
+		}
 
-        // GET: Oranges
-        public async Task<IActionResult> Index()
-        {
-            return View(await _context.Orange.ToListAsync());
-        }
+		// GET: Oranges
+		public async Task<IActionResult> Index()
+		{
+			return View(await _context.Orange.ToListAsync());
+		}
 
-        // GET: Oranges/Details/5
-        public async Task<IActionResult> Details(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
+		// GET: Oranges/Details/5
+		public async Task<IActionResult> Details(int? id)
+		{
+			if (id == null)
+			{
+				return NotFound();
+			}
 
-            var orange = await _context.Orange
-                .FirstOrDefaultAsync(m => m.Id == id);
-            if (orange == null)
-            {
-                return NotFound();
-            }
+			var orange = 
+				await _context.Orange.FirstOrDefaultAsync(m => m.Id == id);
 
-            return View(orange);
-        }
+			if (orange == null)
+			{
+				return NotFound();
+			}
 
-        // GET: Oranges/Create
-        public IActionResult Create()
-        {
-            return View();
-        }
+			return View(orange);
+		}
 
-        // POST: Oranges/Create
-        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
-        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("Id,Name,Farm,HarvestDate,Weight")] Orange orange)
-        {
-            if (ModelState.IsValid)
-            {
-                _context.Add(orange);
-                await _context.SaveChangesAsync();
-                return RedirectToAction(nameof(Index));
-            }
-            return View(orange);
-        }
+		// GET: Oranges/Create
+		public IActionResult Create()
+		{
+			return View();
+		}
 
-        // GET: Oranges/Edit/5
-        public async Task<IActionResult> Edit(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
+		// POST: Oranges/Create
+		// To protect from overposting attacks, enable the specific properties you want to bind to, for 
+		// more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public async Task<IActionResult> Create([Bind("Id,Name,Farm,HarvestDate,Weight")] Orange orange)
+		{
+			if (ModelState.IsValid)
+			{
+				_context.Add(orange);
+				await _context.SaveChangesAsync();
+				return RedirectToAction(nameof(Index));
+			}
+			return View(orange);
+		}
 
-            var orange = await _context.Orange.FindAsync(id);
-            if (orange == null)
-            {
-                return NotFound();
-            }
-            return View(orange);
-        }
+		// GET: Oranges/Edit/5
+		public async Task<IActionResult> Edit(int? id)
+		{
+			if (id == null)
+			{
+				return NotFound();
+			}
 
-        // POST: Oranges/Edit/5
-        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
-        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("Id,Name,Farm,HarvestDate,Weight")] Orange orange)
-        {
-            if (id != orange.Id)
-            {
-                return NotFound();
-            }
+			var orange = await _context.Orange.FindAsync(id);
+			if (orange == null)
+			{
+				return NotFound();
+			}
+			return View(orange);
+		}
 
-            if (ModelState.IsValid)
-            {
-                try
-                {
-                    _context.Update(orange);
-                    await _context.SaveChangesAsync();
-                }
-                catch (DbUpdateConcurrencyException)
-                {
-                    if (!OrangeExists(orange.Id))
-                    {
-                        return NotFound();
-                    }
-                    else
-                    {
-                        throw;
-                    }
-                }
-                return RedirectToAction(nameof(Index));
-            }
-            return View(orange);
-        }
+		// POST: Oranges/Edit/5
+		// To protect from overposting attacks, enable the specific properties you want to bind to, for 
+		// more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public async Task<IActionResult> Edit(int id, [Bind("Id,Name,Farm,HarvestDate,Weight")] Orange orange)
+		{
+			if (id != orange.Id)
+			{
+				return NotFound();
+			}
 
-        // GET: Oranges/Delete/5
-        public async Task<IActionResult> Delete(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
+			if (ModelState.IsValid)
+			{
+				try
+				{
+					_context.Update(orange);
+					await _context.SaveChangesAsync();
+				}
+				catch (DbUpdateConcurrencyException)
+				{
+					if (!OrangeExists(orange.Id))
+					{
+						return NotFound();
+					}
+					else
+					{
+						throw;
+					}
+				}
+				return RedirectToAction(nameof(Index));
+			}
+			return View(orange);
+		}
 
-            var orange = await _context.Orange
-                .FirstOrDefaultAsync(m => m.Id == id);
-            if (orange == null)
-            {
-                return NotFound();
-            }
+		// GET: Oranges/Delete/5
+		public async Task<IActionResult> Delete(int? id)
+		{
+			if (id == null)
+			{
+				return NotFound();
+			}
 
-            return View(orange);
-        }
+			var orange = await _context.Orange
+				.FirstOrDefaultAsync(m => m.Id == id);
+			if (orange == null)
+			{
+				return NotFound();
+			}
 
-        // POST: Oranges/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> DeleteConfirmed(int id)
-        {
-            var orange = await _context.Orange.FindAsync(id);
-            _context.Orange.Remove(orange);
-            await _context.SaveChangesAsync();
-            return RedirectToAction(nameof(Index));
-        }
+			return View(orange);
+		}
 
-        private bool OrangeExists(int id)
-        {
-            return _context.Orange.Any(e => e.Id == id);
-        }
-    }
+		// POST: Oranges/Delete/5
+		[HttpPost, ActionName("Delete")]
+		[ValidateAntiForgeryToken]
+		public async Task<IActionResult> DeleteConfirmed(int id)
+		{
+			var orange = await _context.Orange.FindAsync(id);
+			_context.Orange.Remove(orange);
+			await _context.SaveChangesAsync();
+			return RedirectToAction(nameof(Index));
+		}
+
+		private bool OrangeExists(int id)
+		{
+			return _context.Orange.Any(e => e.Id == id);
+		}
+	}
 }
--- OrangeJuice/Migrations/OrangeJuiceContextModelSnapshot.cs
+++ OrangeJuice/Migrations/OrangeJuiceContextModelSnapshot.cs
@@ -8,41 +8,41 @@ using OrangeJuice.Data;
 
 namespace OrangeJuice.Migrations
 {
-    [DbContext(typeof(OrangeJuiceContext))]
-    partial class OrangeJuiceContextModelSnapshot : ModelSnapshot
-    {
-        protected override void BuildModel(ModelBuilder modelBuilder)
-        {
+	[DbContext(typeof(OrangeJuiceContext))]
+	partial class OrangeJuiceContextModelSnapshot : ModelSnapshot
+	{
+		protected override void BuildModel(ModelBuilder modelBuilder)
+		{
 #pragma warning disable 612, 618
-            modelBuilder
-                .HasAnnotation("ProductVersion", "3.1.6")
-                .HasAnnotation("Relational:MaxIdentifierLength", 128)
-                .HasAnnotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn);
+			modelBuilder
+				.HasAnnotation("ProductVersion", "3.1.6")
+				.HasAnnotation("Relational:MaxIdentifierLength", 128)
+				.HasAnnotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn);
 
-            modelBuilder.Entity("OrangeJuice.Models.Orange", b =>
-                {
-                    b.Property<int>("Id")
-                        .ValueGeneratedOnAdd()
-                        .HasColumnType("int")
-                        .HasAnnotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn);
+			modelBuilder.Entity("OrangeJuice.Models.Orange", b =>
+				{
+					b.Property<int>("Id")
+						.ValueGeneratedOnAdd()
+						.HasColumnType("int")
+						.HasAnnotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn);
 
-                    b.Property<string>("Farm")
-                        .HasColumnType("nvarchar(max)");
+					b.Property<string>("Farm")
+						.HasColumnType("nvarchar(max)");
 
-                    b.Property<DateTime>("HarvestDate")
-                        .HasColumnType("datetime2");
+					b.Property<DateTime>("HarvestDate")
+						.HasColumnType("datetime2");
 
-                    b.Property<string>("Name")
-                        .HasColumnType("nvarchar(max)");
+					b.Property<string>("Name")
+						.HasColumnType("nvarchar(max)");
 
-                    b.Property<decimal>("Weight")
-                        .HasColumnType("decimal(18,2)");
+					b.Property<decimal>("Weight")
+						.HasColumnType("decimal(18,2)");
 
-                    b.HasKey("Id");
+					b.HasKey("Id");
 
-                    b.ToTable("Orange");
-                });
+					b.ToTable("Orange");
+				});
 #pragma warning restore 612, 618
-        }
-    }
+		}
+	}
 }
