86895951497fa8356bc39e5510d996930c5b6cba
ctrl-greg <65520831+ctrl-greg@users.noreply.github.com>
2020-08-09 13:27:49 -0400

    Add a View

---
+++ OrangeJuice/Controllers/HelloWorldController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+namespace OrangeJuice.Controllers 
+{
+	public class HelloWorldController : Controller
+	{
+		// GET /HelloWorld
+		public IActionResult Index()
+		{
+			return View();
+		}
+
+		// Get /HelloWorld/Welcome
+		public IActionResult Welcome(string name, int numTimes = 4)
+		{
+			ViewData["Name"] = name;
+			ViewData["NumTimes"] = numTimes;
+
+			return View();
+		}
+	}
+}
