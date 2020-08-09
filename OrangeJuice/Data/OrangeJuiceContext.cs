3f9a05e9d48cff587b8f999ddaa5ed52712bb325
ctrl-greg <65520831+ctrl-greg@users.noreply.github.com>
2020-08-09 13:40:57 -0400

    created created ORange model and OrangeJuiceContext

---
+++ OrangeJuice/Data/OrangeJuiceContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using Microsoft.EntityFrameworkCore;
+using OrangeJuice.Models;
+
+namespace OrangeJuice.Data
+{
+	public class OrangeJuiceContext : DbContext
+	{
+		public OrangeJuiceContext(DbContextOptions<OrangeJuiceContext> options) : base(options)
+		{
+
+		}
+
+		public DbSet<Orange> Orange { get; set; }
+	}
+}
---
+++ OrangeJuice/Models/Orange.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace OrangeJuice.Models
+{
+	public class Orange
+	{
+		public int Id { get; set; }
+		
+		public string Name { get; set; }
+		
+		public string Farm { get; set; }
+		
+		// Specifies DateTime to just include date
+		[DataType(DataType.Date)]
+		public DateTime HarvestDate { get; set; }
+
+		public decimal Weight { get; set; }
+	}
+}
