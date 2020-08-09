b138f4b4356afe707140100ed6e87959fc4eeaee
ctrl-greg <65520831+ctrl-greg@users.noreply.github.com>
2020-08-09 14:39:47 -0400

    initial database migration

---
+++ OrangeJuice/Migrations/20200809182407_InitialCreate.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace OrangeJuice.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Orange",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(nullable: true),
+                    Farm = table.Column<string>(nullable: true),
+                    HarvestDate = table.Column<DateTime>(nullable: false),
+                    Weight = table.Column<decimal>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Orange", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Orange");
+        }
+    }
+}
